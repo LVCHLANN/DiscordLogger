78e7436972c720898baf880204bc134ab3743345
Lachlan <lachie@schmurgon.net>
2024-12-07 00:43:49 +1100

    Added back PlayerChatLogs
    Added back the ability to log Player Chat, something I accidentally removed, however in the process now if a server name is specified it will be in the message twice

--- src/main/java/com/DiscordLogger/EventListener.java
+++ src/main/java/com/DiscordLogger/EventListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerCommandEvent;
@@ -20,16 +21,24 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (plugin.getConfig().getBoolean("log-player-join", true)) {
-            plugin.logToDiscord("Player Join", event.getPlayer().getName() + " joined the server.");
-            plugin.getLogger().info(event.getPlayer().getName() + " joined the server.");
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? event.getPlayer().getName() + " joined the server."
+                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " joined the server.";
+
+            plugin.logToDiscord("Player Join", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (plugin.getConfig().getBoolean("log-player-quit", true)) {
-            plugin.logToDiscord("Player Quit", event.getPlayer().getName() + " left the server.");
-            plugin.getLogger().info(event.getPlayer().getName() + " left the server.");
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? event.getPlayer().getName() + " left the server."
+                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " left the server.";
+
+            plugin.logToDiscord("Player Quit", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
 
@@ -37,32 +46,58 @@ public class EventListener implements Listener {
     public void onServerCommand(ServerCommandEvent event) {
         if (plugin.getConfig().getBoolean("log-server-command", true)) {
             String sender = event.getSender().getName();
-            plugin.logToDiscord("Server Command", "Command executed: /" + event.getCommand() + " by " + sender);
-            plugin.getLogger().info("Command executed: /" + event.getCommand() + " by " + sender);
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? "Command executed: /" + event.getCommand() + " by " + sender
+                    : "[" + plugin.getServerName() + "] Command executed: /" + event.getCommand() + " by " + sender;
+
+            plugin.logToDiscord("Server Command", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (plugin.getConfig().getBoolean("log-block-break", false)) {
-            plugin.logToDiscord("Block Break", event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation());
-            plugin.getLogger().info(event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation());
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation()
+                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation();
+
+            plugin.logToDiscord("Block Break", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         if (plugin.getConfig().getBoolean("log-block-place", false)) {
-            plugin.logToDiscord("Block Place", event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation());
-            plugin.getLogger().info(event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation());
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation()
+                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation();
+
+            plugin.logToDiscord("Block Place", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (plugin.getConfig().getBoolean("log-player-death", false)) {
-            plugin.logToDiscord("Player Death", event.getDeathMessage());
-            plugin.getLogger().info(event.getDeathMessage());
+            String logMessage = plugin.getServerName().isEmpty()
+                    ? event.getDeathMessage()
+                    : "[" + plugin.getServerName() + "] " + event.getDeathMessage();
+
+            plugin.logToDiscord("Player Death", logMessage);
+            plugin.getLogger().info(logMessage);
         }
     }
+
+    @EventHandler
+    public void onPlayerChat(PlayerChatEvent event) {
+        if (plugin.getConfig().getBoolean("log-player-chat", true)) {
+            String serverNamePrefix = "[" + plugin.getServerName() + "] ";
+            plugin.logToDiscord("Player Chat", serverNamePrefix + event.getPlayer().getName() + ": " + event.getMessage());
+            plugin.getLogger().info(serverNamePrefix + event.getPlayer().getName() + ": " + event.getMessage());
+        }
+    }
+
 }
