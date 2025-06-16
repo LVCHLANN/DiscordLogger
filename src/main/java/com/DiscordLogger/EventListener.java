b5100b6f2ad594faec6e5f547c592330c1cbac57
Lachlan <lachie@schmurgon.net>
2025-06-16 12:40:16 +1000

    1.0.4 Update

--- src/main/java/com/DiscordLogger/EventListener.java
+++ src/main/java/com/DiscordLogger/EventListener.java
@@ -5,7 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.ServerCommandEvent;
@@ -21,10 +21,7 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (plugin.getConfig().getBoolean("log-player-join", true)) {
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? event.getPlayer().getName() + " joined the server."
-                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " joined the server.";
-
+            String logMessage = event.getPlayer().getName() + " joined the server.";
             plugin.logToDiscord("Player Join", logMessage);
             plugin.getLogger().info(logMessage);
         }
@@ -33,24 +30,26 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (plugin.getConfig().getBoolean("log-player-quit", true)) {
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? event.getPlayer().getName() + " left the server."
-                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " left the server.";
-
+            String logMessage = event.getPlayer().getName() + " left the server.";
             plugin.logToDiscord("Player Quit", logMessage);
             plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
-    public void onServerCommand(ServerCommandEvent event) {
-        if (plugin.getConfig().getBoolean("log-server-command", true)) {
-            String sender = event.getSender().getName();
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? "Command executed: /" + event.getCommand() + " by " + sender
-                    : "[" + plugin.getServerName() + "] Command executed: /" + event.getCommand() + " by " + sender;
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (plugin.getConfig().getBoolean("log-player-chat", true)) {
+            String logMessage = event.getPlayer().getName() + ": " + event.getMessage();
+            plugin.logToDiscord("Chat", logMessage);
+            plugin.getLogger().info(logMessage);
+        }
+    }
 
-            plugin.logToDiscord("Server Command", logMessage);
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        if (plugin.getConfig().getBoolean("log-player-death", true)) {
+            String logMessage = event.getDeathMessage();
+            plugin.logToDiscord("Death", logMessage);
             plugin.getLogger().info(logMessage);
         }
     }
@@ -58,10 +57,8 @@ public class EventListener implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (plugin.getConfig().getBoolean("log-block-break", false)) {
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation()
-                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " broke a block at " + event.getBlock().getLocation();
-
+            String logMessage = event.getPlayer().getName() + " broke " +
+                    event.getBlock().getType() + " at " + event.getBlock().getLocation();
             plugin.logToDiscord("Block Break", logMessage);
             plugin.getLogger().info(logMessage);
         }
@@ -70,34 +67,19 @@ public class EventListener implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         if (plugin.getConfig().getBoolean("log-block-place", false)) {
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation()
-                    : "[" + plugin.getServerName() + "] " + event.getPlayer().getName() + " placed a block at " + event.getBlock().getLocation();
-
+            String logMessage = event.getPlayer().getName() + " placed " +
+                    event.getBlock().getType() + " at " + event.getBlock().getLocation();
             plugin.logToDiscord("Block Place", logMessage);
             plugin.getLogger().info(logMessage);
         }
     }
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        if (plugin.getConfig().getBoolean("log-player-death", false)) {
-            String logMessage = plugin.getServerName().isEmpty()
-                    ? event.getDeathMessage()
-                    : "[" + plugin.getServerName() + "] " + event.getDeathMessage();
-
-            plugin.logToDiscord("Player Death", logMessage);
+    public void onServerCommand(ServerCommandEvent event) {
+        if (plugin.getConfig().getBoolean("log-server-command", false)) {
+            String logMessage = "Console ran command: /" + event.getCommand();
+            plugin.logToDiscord("Server Command", logMessage);
             plugin.getLogger().info(logMessage);
         }
     }
-
-    @EventHandler
-    public void onPlayerChat(PlayerChatEvent event) {
-        if (plugin.getConfig().getBoolean("log-player-chat", true)) {
-            String serverNamePrefix = "[" + plugin.getServerName() + "] ";
-            plugin.logToDiscord("Player Chat", serverNamePrefix + event.getPlayer().getName() + ": " + event.getMessage());
-            plugin.getLogger().info(serverNamePrefix + event.getPlayer().getName() + ": " + event.getMessage());
-        }
-    }
-
 }
